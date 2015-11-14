6050e14f8d928d1368be2b824a15254702aaf12f
Judgewooden <douwe.jong@gmail.com>
2015-11-14 17:32:36 +0100

    Added Arduino Monitor 1

---
+++ runArduinoMonitor1.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+python sensorCollectArduinoMonitor1.py
+
---
+++ sensorCollectArduinoMonitor1.py
@@ -0,0 +1,65 @@
+#!/usr/bin/python
+import os
+import time
+import datetime
+import requests
+import json
+from datetime import datetime
+#import sys
+import mysql.connector
+
+# Get config info
+try:
+#   TODO clean this up and DO it properly
+    f = open('/home/pi/source/JPHmonitor/.sqlpassword')
+    sqlpassword=f.read().strip()
+    f.close
+except:
+    print ("Unexpected error: opening .sqlpassword")
+    raise
+
+try:
+ cnx = mysql.connector.connect(user='root', password=sqlpassword,
+                              host='localhost',
+                              database='Sensors')
+except:
+ print ("Cannot Connect to SQL")
+ raise
+
+# GLOBAL
+add_temp = ("INSERT INTO ArduinoMonitor1 "
+               "(Timestamp, Power, FlowPerSecond, LitersPerMinute, OverrideTime, CurrentTime)"
+               "VALUES (%s, %s, %s, %s, %s, %s)")
+url = "http://192.168.8.11/X"
+
+while True:
+    tnow = datetime.now()
+
+    try:
+        response=requests.get(url, timeout=(0.5, 10.0))
+        if response.headers["content-type"] != "application/json":
+            raise WrongContent(response=response)
+        try:
+            json_data = json.loads(response.text)
+        except ValueError:
+            raise WrongContent(response=response)
+
+        print json.dumps(json_data)
+        lastValue=(tnow,
+            json_data["Power"],
+            json_data["FlowPerSecond"],
+            json_data["LitersPerSecond"],
+            json_data["OverrideTime"],
+            json_data["CurrentTime"])
+
+        print lastValue
+        cursor = cnx.cursor()
+        cursor.execute(add_temp, lastValue)
+        cnx.commit()
+
+    except requests.exceptions.ConnectionError as e:
+        print "server not found."
+    except requests.exceptions.ReadTimeout as t:
+        print "Waited too long between bytes."
+
+    time.sleep(5)
