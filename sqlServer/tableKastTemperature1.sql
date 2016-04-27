5ad1ce8498ed064b15ec832e6f392e7feb76197b
Judgewooden <douwe.jong@gmail.com>
2016-04-27 17:31:39 +0200

    Adding Adafruit

--- sqlServer/tableKastTemperature1.sql
+++ sqlServer/tableKastTemperature1.sql
@@ -4,7 +4,7 @@ DROP TABLE IF EXISTS KastTemperature1;
 /* The temperature of the AirSensors outside */
 CREATE TABLE `KastTemperature1` (
 	`Timestamp` timestamp NOT NULL,
-	`Value` numeric(7,3),
+	`Temperature` numeric(7,3),
 	`Humidity` numeric(7,3),
 	PRIMARY KEY (`Timestamp`),
 	UNIQUE KEY `Timestamp` (`Timestamp`)
